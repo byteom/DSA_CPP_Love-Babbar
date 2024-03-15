3755e71300fc6d559c49c2e791f8855388c29e9a
byteom <123omsinghpipariya@gmail.com>
2024-03-15 12:24:36 +0530

    Update repo

--- Char_Array_Strings/char_array.cpp
+++ Char_Array_Strings/char_array.cpp
@@ -1,10 +1,37 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char ch[100];
+//     cout<<"Enter your name : ";
+//     cin>>ch; // the a input of many charchter at one time, its your choiuce what u entering.
+//      cin>> ch[50]; // taking single charchter
+//     cout<<"Your name is : "<<ch;
+//     return 0;
+// }
+
+//Example for verfiying null char 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char ch[100 ];
+//     cout<<"Enter your name : ";
+//     cin>>ch;
+//     for(int i=0;i<7;i++){
+//         cout<<"index "<<i<<" value "<<ch[i]<<endl;
+//     }
+
+//     int value =(int)ch[5];
+//     int value1 =(int)ch[6];
+//     cout<<"Value is : "<<value<<endl; // ASCII value of ch[5] will be store in integer variable 'value' , its null = 0
+//     cout<<"Value is : "<<value1<<endl; // ASCII value of ch[6] will be store in integer variable 'value' , its null = 0
+//     return 0;
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
     char ch[100];
-    cout<<"Enter your name : ";
-    cin>>ch; // the a input of many charchter at one time, its your choiuce what u entering.
-     cin>> ch[50]; // taking single charchter
-    cout<<"Your name is : "<<ch;
-    return 0;
+    cin>> ch;
+    cout<<ch;
 }
