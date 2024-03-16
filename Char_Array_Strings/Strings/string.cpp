49dbdd2a7b9398b605fe8f192fbb8d474e50e995
byteom <123omsinghpipariya@gmail.com>
2024-03-16 13:34:04 +0530

    update

--- Char_Array_Strings/Strings/string.cpp
+++ Char_Array_Strings/Strings/string.cpp
@@ -4,7 +4,15 @@ using namespace std;
 int main(){
     string str;
    // cin>>str;
-    getline(cin, str);
+   getline(cin, str);
     cout<<str;
+    cout<<"length :"<<str.length()<<endl; // length of the string
+    cout<<"is empty :"<<str.empty(); // check if the string is empty
+    cout<<"is not empty :"<<!str.empty()<<endl; // check if the string is not empty
+    str.push_back('A');
+    //cout<<str;
+    str.pop_back();
+   // cout<<str;
+    cout<<str.substr(0,6)<<endl;  // print a substring from index 0 to
     return 0;
 }
---
+++ Char_Array_Strings/Strings/stringComparefn.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool compareString(string a, string b){
+    if(a.length()==b.length()){
+        int j=0;
+        for(int i=0;i<a.length();i++){
+            if(a[i]!=b[j]){
+                return false;
+            }j++;
+        }
+    }else{
+        return false;
+    }return true;
+}
+int main(){
+    string a =  "love";
+    string b=  "love";
+
+    bool result =compareString(a,b);
+    if(result==true){
+        cout<<"true";
+    }else{
+        cout<<"false";
+    }
+
+    return 0;
+}
